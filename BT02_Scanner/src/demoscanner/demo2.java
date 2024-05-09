a9daf00eeeefe50c780555068f6cbf5e069c265d
TomOutfit <167419245+TomOutfit@users.noreply.github.com>
2024-05-09 16:47:10 +0700

    Update demo2.java

--- BT02_Scanner/src/demoscanner/demo2.java
+++ BT02_Scanner/src/demoscanner/demo2.java
@@ -8,13 +8,13 @@ public class demo2 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhap vao ho ten:");
         String name = sc.nextLine();
-        String tuoi = "19";
-        int age = Integer.parseInt(tuoi);
+        System.out.print("Nhập tuổi: );
+        int age = Integer.parseInt(sc.nextLine());
         System.out.println(age);
         System.out.println("Hãy nhập địa chỉ của bạn: ");
         String where = sc.nextLine();
-        String c = "0935649095";
-        double phone = Double.parseDouble(c);
+        System.out.println("Hãy nhập Sđt của bạn: ");
+        double phone = Double.parseDouble(sc.nextLine());
         System.out.println(c);
         System.out.println("Hãy nhập chuyên ngành của bạn: ");
         String major = sc.nextLine();
